dedfbe82da7e3c65c142b228c11ecb26ebd256c9
bzirs <bzirs@outlook.com>
2022-12-22 21:26:42 +0800

    init: 🚀 初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: bzirs
+ * @Date: 2022-12-22 21:15:29
+ * @LastEditors: bzirs
+ * @LastEditTime: 2022-12-22 21:24:38
+ * @FilePath: /vue2-itcast-headlines/src/App.vue
+ * @Description: App.vue
+ *
+ * Copyright (c) 2022 by bzirs, All Rights Reserved.
+-->
+<template>
+  <div id="app">
+    <!-- 一级路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+/**
+ * @Author: bzirs
+ * @Date: 2022-12-22 21:15:29
+ * @LastEditors: bzirs
+ * @LastEditTime: 2022-12-22 21:17:16
+ * @FilePath: /vue2-itcast-headlines/src/router/index.js
+ * @Description: 路由
+ * @
+ * @Copyright (c) 2022 by bzirs, All Rights Reserved.
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = []
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
