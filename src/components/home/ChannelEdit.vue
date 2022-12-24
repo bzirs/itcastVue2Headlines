0945cde7d623f55bd90feb3a010aec0f44adc49c
bzirs <bzirs@outlook.com>
2022-12-24 21:26:20 +0800

    feat: ✨  完成编辑频道小叉叉的显示隐藏

--- src/components/home/ChannelEdit.vue
+++ src/components/home/ChannelEdit.vue
@@ -2,18 +2,14 @@
  * @Author: bzirs
  * @Date: 2022-12-24 15:51:39
  * @LastEditors: bzirs
- * @LastEditTime: 2022-12-24 18:23:49
+ * @LastEditTime: 2022-12-24 21:23:20
  * @FilePath: /vue2-itcast-headlines/src/components/home/ChannelEdit.vue
  * @Description: 使用vant组件实现
  *
  * Copyright (c) 2022 by bzirs, All Rights Reserved.
 -->
 <template>
-  <van-popup
-    v-model="show"
-    get-container="#app"
-    class="edit-channel"
-  >
+  <van-popup v-model="show" get-container="#app" class="edit-channel">
     <!-- 顶部栏 -->
     <van-nav-bar
       placeholder
@@ -27,23 +23,22 @@
     </van-nav-bar>
 
     <!-- 我的频道 -->
-    <channel-edit-item>
+    <channel-edit-item :flag="flag" :list="list">
       <template #left>
         <span class="custom-title">我的频道</span>
         <span class="go-channel">点击进入频道</span>
       </template>
       <template #right>
-        <span>编辑</span>
+        <span @click="flag = !flag">编辑</span>
       </template>
     </channel-edit-item>
 
     <!-- 添加频道 -->
-    <channel-edit-item>
+    <channel-edit-item :list="notSelectChannel">
       <template #left>
         <span class="custom-title">点击添加更多频道：</span>
       </template>
     </channel-edit-item>
-
   </van-popup>
 </template>
 
@@ -64,7 +59,10 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      // 显示左上叉叉
+      flag: false
+    }
   },
   async created () {},
   mounted () {},
--- src/components/home/ChannelEditItem.vue
+++ src/components/home/ChannelEditItem.vue
@@ -2,35 +2,36 @@
  * @Author: bzirs
  * @Date: 2022-12-24 16:52:02
  * @LastEditors: bzirs
- * @LastEditTime: 2022-12-24 18:18:50
+ * @LastEditTime: 2022-12-24 21:22:04
  * @FilePath: /vue2-itcast-headlines/src/components/home/ChannelEditItem.vue
  * @Description:
  *
  * Copyright (c) 2022 by bzirs, All Rights Reserved.
 -->
 <template>
- <div>
-   <!-- 频道顶部 -->
-  <van-cell>
-    <template #title>
-      <slot name="left"></slot>
-    </template>
+  <div>
+    <!-- 频道顶部 -->
+    <van-cell>
+      <template #title>
+        <slot name="left"></slot>
+      </template>
 
-    <template #default>
-      <slot name="right"></slot>
-    </template>
-  </van-cell>
+      <template #default>
+        <slot name="right"></slot>
+      </template>
+    </van-cell>
 
-  <!-- 频道列表 -->
-  <!-- <van-grid :gutter="10">
-    <van-grid-item
-      v-for="item in list"
-      :key="item.id"
-      icon="photo-o"
-      text="文字"
-    />
-  </van-grid> -->
- </div>
+    <!-- 频道列表 -->
+    <van-grid :gutter="10">
+      <van-grid-item v-for="item in list" :key="item.id" :text="item.name">
+        <template #icon>
+          <div class="icon-right" v-if="flag">
+            <van-icon name="cross" />
+          </div>
+        </template>
+      </van-grid-item>
+    </van-grid>
+  </div>
 </template>
 
 <script>
@@ -40,10 +41,14 @@ export default {
   props: {
     list: {
       type: Array
+    },
+    flag: {
+      type: Boolean
     }
   },
   data () {
-    return {}
+    return {
+    }
   },
   async created () {},
   mounted () {},
@@ -56,5 +61,23 @@ export default {
 }
 </script>
 
-<style scoped lang=''>
+<style scoped lang='scss'>
+::v-deep .van-grid-item__content--center {
+  position: relative;
+  background-color: #fafafa;
+  height: 36px;
+  .van-grid-item__icon-wrapper {
+    position: static;
+  }
+  .icon-right {
+    position: absolute;
+    right: 0;
+    top: -2px;
+    // display: none;
+  }
+}
+::v-deep i.van-icon {
+  color: rgb(207, 207, 207);
+  font-size: 12px;
+}
 </style>
